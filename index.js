a4bf9f894c07fc468d92b98ddec63d58ba5221a8
PauliCZ44 <stastnyp@seznam.cz>
2020-08-28 12:00:57 +0200

    3.8* + 3.7: Phonebook backend step8 + step7

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-/*
-3.3: Phonebook backend step3 - done
-*/
-
 const express = require("express");  //adding express
 const { response } = require("express");
 const app = express()                // create express aplication
+const morgan = require('morgan')    //morgan logging api
+//morgan('tiny');   //Using a predefined format string 
 
-app.use(express.json())                 //JSON parser
+
+
+app.use(express.json())                 //JSON parser - must be declared before all .get / .post etc.
+
+//define my token for name and number logging
+morgan.token('nameAndNumber', function (req, res) { return (
+  JSON.stringify({
+   name: req.body['name'],
+   number: req.body['number']
+  })
+  )})
+
+app.use(morgan("tiny",  {  // output :method :url :status :res[content-length] - :response-time ms  
+  skip: function (req, res) { return res.statusCode === 201 }
+}))
+
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :nameAndNumber",  {
+  skip: function (req, res) { return res.statusCode !== 201 }
+}))
+
 
 let persons = [
     { name: 'Arto Hellas', number: '040-123456', id:1 },
@@ -19,57 +37,65 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
 
-  app.get('/info', (request, response) => {
-    const date = new Date()
-    response.send(`<p>Phonebook has info fo ${persons.length} people<p>  <p>${date}<p>  `)
-  })
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`<p>Phonebook has info fo ${persons.length} people<p>  <p>${date}<p>  `)
+})
 
-  app.get('/api/persons/:id', (request, response) => {
-      const id = Number(request.params.id)
-      console.log(id)
-      const pers = persons.find(p => p.id == id)    // try to find a person based on id in request
-      if (!pers) {
-          response.status(404).end()
-      } else {
-        response.json(pers)
-      }
-
-  })
-
-  app.delete("/api/persons/:id", (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     console.log(id)
-    persons = persons.filter(p => p.id !== id)    // FILTER PERSONS BASED ON ID
-    response.status(204).end()
-  })
-
-  app.post("/api/persons", (request, response) => {
-    const newId = Math.floor(Math.random()*10000)
-    // let nameAlreadyExist = persons.findIndex(p => p.name === request.body.name) //returns -1 if person is not found  //better code with .some
-
-    if (persons.some(e => e.name == request.body.name)) {  //if in persons array is SOME (at least 1) name that is == to req.body.name then this is true
-      return response.status(400).json({
-        error: "Name must be unique"
-      })
-    }
-    if (request.body.name === "" || request.body.number === "") {
-      return response.status(400).json({
-        error: "Number and name should be included"
-      })
+    const pers = persons.find(p => p.id == id)    // try to find a person based on id in request
+    if (!pers) {
+        response.status(404).end(`The id of "${id}" does not exists`)
+    } else {
+      response.json(pers)
     }
 
-    const newPerson = {
-      name: request.body.name,
-      number: request.body.number,
-      id: newId
-    }
-    persons = persons.concat(newPerson)
-    response.json(persons)
-  })
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  persons = persons.filter(p => p.id !== id)    // FILTER PERSONS BASED ON ID
+  response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const newId = Math.floor(Math.random()*10000)
+  // let nameAlreadyExist = persons.findIndex(p => p.name === request.body.name) //returns -1 if person is not found  //better code with .some
+
+  if (persons.some(e => e.name == request.body.name)) {  //if in persons array is SOME (at least 1) name that is == to req.body.name then this is true
+    return response.status(400).json({
+      error: "Name must be unique"
+    })
+  }
+  if (request.body.name === "" || request.body.number === "") {
+    return response.status(400).json({
+      error: "Number and name should be included"
+    })
+  }
+
+  const newPerson = {
+    name: request.body.name,
+    number: request.body.number,
+    id: newId
+  }
+  persons = persons.concat(newPerson)
+  response.status(201).json(persons)
+})
+
+const unknownEndpoint = (request, response, next) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
